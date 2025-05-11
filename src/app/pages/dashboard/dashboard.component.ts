b1d018cbdd80603646923e9f42608b1807d5540c
Asadnaveed47 <asad.naveed@hypernymbiz.com>
2025-05-11 15:10:01 +0500

    card fixed

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { BookingFormComponent } from "./booking-form/booking-form.component";
 import { CommonModule } from '@angular/common';
 import { ColumnRotatedComponent } from "../../shared/column-rotated/column-rotated.component";
 import { PieChartComponent } from "../../shared/pie-chart/pie-chart.component";
+import { ApiserviceService } from '../../services/apiservice/apiservice.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,6 +15,38 @@ import { PieChartComponent } from "../../shared/pie-chart/pie-chart.component";
 })
 export class DashboardComponent {
   showModal = false;
+  cardDataMap: any = {};
+  graphData: any[] = [];
+  constructor(
+    private apiService: ApiserviceService,
+
+  ){
+  }
+
+  ngOnInit(){
+  this.getCards();
+  this.getgraphs();
+  }
+
+  getCards() {
+    const url = `${environment.baseUrl}/api/v1/analytics/cards?dashboard_id=MD&business_id=5`;
+    this.apiService.get(url).subscribe(response => {
+      if (response.status === 1000) {
+        this.cardDataMap = response.data.reduce((acc: any, item: any) => {
+          acc[item.code] = item.data;
+          return acc;
+        }, {});
+      }
+    });
+  }
+  getgraphs() {
+    const url = `${environment.baseUrl}/api/v1/analytics/graphs?dashboard_id=MD&business_id=5`;
+    this.apiService.get(url).subscribe(response => {
+      if (response.status === 1000) {
+        this.graphData = response.data;
+      }
+    });
+  }
 
   addBooking() {
     this.showModal = true;
