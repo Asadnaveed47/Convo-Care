f017f30e31fd926eebba26f32faf9f0fb80169ff
Asadnaveed47 <asad.naveed@hypernymbiz.com>
2025-05-12 16:54:06 +0500

    changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth'; // If using Auth
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'; // If using Firestore
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
+    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
     providePrimeNG({
         theme: {
             preset: Aura,
@@ -15,5 +19,7 @@ export const appConfig: ApplicationConfig = {
                 darkModeSelector: false || 'none',
             }
         }
-    })]
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), // Initialize Firebase app
+    ]
 };
